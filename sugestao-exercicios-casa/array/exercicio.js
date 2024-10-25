64a8c90b581a33fde362a5d0c75b777e00ee7e9a
mflilian <mayhhara.morais@grupoboticario.com.br>
2024-10-24 21:07:13 -0300

    adiciona sugestao de atividade pra casa

---
+++ sugestao-exercicios-casa/array/exercicio.js
@@ -0,0 +1,11 @@
+// 1. Crie um array com 3 cores favoritas e imprima cada cor no console
+
+// 2. Adicione um novo valor no array na quarta posição e imprima o array completo no console
+
+// 3. Substitua a segunda cor do array e imprima o array completo no console
+
+// 4. Imprima o tamanho do array no console (sem usar métodos, usando length)
+
+// 5. Crie um array com 5 números e calcule a soma dos três primeiros números
+
+// 6. Acesse o último valor do array sem usar métodos e imprima no console
---
+++ sugestao-exercicios-casa/objetos/exercicio.js
@@ -0,0 +1,14 @@
+// 1. Crie um objeto para representar um livro com as seguintes propriedades:
+// Título, Autor, Ano de Publicação. Imprima o objeto no console.
+
+// 2. Acesse e imprima o título do livro no console
+
+// 3. Atualize o valor da propriedade 'anoPublicacao' e imprima o objeto atualizado
+
+// 4. Adicione uma nova propriedade chamada 'editora' ao objeto e imprima o objeto completo
+
+// 5. Crie um objeto representando um carro com as propriedades: marca, modelo, ano, e cor. Imprima o carro no console.
+
+// 6. Atualize a cor do carro para 'Prata' e imprima o carro atualizado
+
+// 7. Acesse e imprima o ano e o modelo do carro separadamente
